486ac807c689bdd82546493e91f6650fbbcd21b0
anton9760 <anton2610@mail.ru>
2024-02-17 22:26:36 +0300

    Шаг 3.2

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
+import sys 
+sys.path.append('/home/anton9760/python-projects/python-project-49/brain_games')
+import cli
+    
+    
+cli.welcome_user()
 
-
-def main():
-    pass
-
-
-
-
-if __name__ == '__main__':
-    main()
