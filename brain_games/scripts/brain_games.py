5c255474b244131818cddbb9782f82cb2c6444aa
anton9760 <anton2610@mail.ru>
2024-02-18 09:40:25 +0300

    Шаг 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-import sys 
+import sys
 sys.path.append('/home/anton9760/python-projects/python-project-49/brain_games')
 import cli
-    
-    
+
+
 cli.welcome_user()
 
+
+def main():
+    pass
