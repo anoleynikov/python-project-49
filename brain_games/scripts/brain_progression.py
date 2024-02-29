658c3c517822ec5e966b2cf9510b865182f26eb6
anton9760 <anton2610@mail.ru>
2024-02-29 20:11:38 +0300

    make files for brain-progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.game_engine import run_game
+import brain_games.games.brain_progression
+
+
+def main():
+    run_game(brain_games.games.brain_progression)
+
+
+if __name__ == '__main__':
+    main()
